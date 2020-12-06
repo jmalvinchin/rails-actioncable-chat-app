9d553c10071b278578bc4ca3010c15eb39aad0ac
Jess Chin <jmalvinchin@gmail.com>
2020-12-06 16:04:48 +0800

    Sample chat room using websockets

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,44 @@
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
+require("jquery")
+
+
+// Uncomment to copy all static images under ../images to the output folder and reference
+// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
+// or the `imagePath` JavaScript helper below.
+//
+// const images = require.context('../images', true)
+// const imagePath = (name) => images(name, true)
+
+import chatRoomChannel from "../channels/chat_room_channel";
+
+$(document).on('turbolinks:load', function () {
+  $("form#set_name").on('submit', function(e){
+    e.preventDefault();
+    let name = $('#add_name').val();
+    sessionStorage.setItem('chat_room_name', name)
+    chatRoomChannel.announce({ name, type: 'join'})
+    $("#modal").css('display', 'none');
+  });
+
+  $("form#send_message").on('submit', function(e){
+    e.preventDefault();
+    let message = $('#message').val();
+    if (message.length > 0) {
+      chatRoomChannel.speak(message);
+      $('#message').val('')
+    }
+  });
+
+  $(window).on('beforeunload', function() {
+    let name = sessionStorage.getItem('chat_room_name')
+    chatRoomChannel.announce({ name, type: 'leave'})
+  });
+})
